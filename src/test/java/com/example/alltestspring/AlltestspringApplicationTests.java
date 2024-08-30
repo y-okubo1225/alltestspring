b81b3aa8b33d10cebd3c7b93aa80f25969efefdf
y-okubo <y.okubo1225@gmail.com>
2024-08-30 15:37:12 +0900

    frist commit

---
+++ src/test/java/com/example/alltestspring/AlltestspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alltestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlltestspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
